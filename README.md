0bddbe7d8ee851b71382e5a44465fe59897ef5a2
brad <34373485+baxtrax@users.noreply.github.com>
2023-11-02 23:30:29 -0400

    integration of login-api

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Deployment-Files
-Holds any files revelent to deployment
+Holds any files revelent to deployment. Scripts will generate the needed kubernetes configuration files to configure and launch the microservices and connect them together.
+
+Resulting Frontend will be accessible at [http://localhost:30082](http://localhost:30082)
 
 # Install
 ```bash
@@ -10,7 +12,10 @@ go install sigs.k8s.io/kind@v0.20.0
 Create a `.env` for each microservice
 ```bash
 touch ./web-api/.web-api-env
+touch ./login-api/.login-api-env
 ```
+See samples in the samples folder for template env configurations for each service
+
 
 # Run
 ```bash
@@ -20,4 +25,21 @@ touch ./web-api/.web-api-env
 # Stop
 ```bash
 ./scripts/stop.sh
+```
+
+# Misc
+View current running pods with
+```bash
+kubectl get pods
+```
+```bash
+kubectl get services
+```
+
+And view logs of each pod with
+```bash
+kubectl describe <pod name>
+```
+```bash
+kubectl logs <pod name>
 ```
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 SCRIPT_DIR="$(pwd)"
+
+# Web API replacement and setup
 APP_PORT=$(grep -oP 'APP_PORT=\K\d+' $SCRIPT_DIR/../web-api/.web-api-env)
 
-# Replace all <APP_PORT> with the actual port number
-sed -e "s:<APP_PORT>:$APP_PORT:g" -e "s:<APP_PORT_SMALL>:${APP_PORT: -2}:g" $SCRIPT_DIR/../web-api/template-web-api-config.yaml > $SCRIPT_DIR/../web-api/web-api-config.yaml
-sed -e "s:<APP_PORT_SMALL>:${APP_PORT: -2}:g" $SCRIPT_DIR/../template-kind-config.yaml > $SCRIPT_DIR/../kind-config.yaml
+sed -e "s:<APP_PORT>:$APP_PORT:g" \
+    -e "s:<APP_PORT_SMALL>:${APP_PORT: -2}:g" \
+    $SCRIPT_DIR/../web-api/template-web-api-config.yaml > $SCRIPT_DIR/../web-api/web-api-config.yaml
 
+sed -e "s:<APP_PORT_SMALL>:${APP_PORT: -2}:g" \
+    $SCRIPT_DIR/../template-kind-config.yaml > $SCRIPT_DIR/../kind-config.yaml
+
+# Login API replacement and setup
+APP_PORT=$(grep -oP 'APP_PORT=\K\d+' $SCRIPT_DIR/../login-api/.login-api-env)
+MONGO_PORT=$(grep -oP 'MONGODB_REDIRECT=\K.*' $SCRIPT_DIR/../login-api/.login-api-env)
+MONGO_URI=$(grep -oP 'MONGODB_URI=\K.*' $SCRIPT_DIR/../login-api/.login-api-env)
+MONGO_USERNAME=$(echo $MONGO_URI | awk -F '://' '{print $2}' | awk -F ':' '{print $1}')
+MONGO_PASSWORD=$(echo $MONGO_URI | awk -F '://' '{print $2}' | awk -F ':' '{print $2}' | awk -F '@' '{print $1}')
+
+sed -e "s:<APP_PORT>:$APP_PORT:g" \
+    -e "s:<MONGO_PORT>:$MONGO_PORT:g" \
+    -e "s/<MONGO_ROOT_USERNAME>/$MONGO_USERNAME/g" \
+    -e "s/<MONGO_ROOT_PASSWORD>/$MONGO_PASSWORD/g" \
+    $SCRIPT_DIR/../login-api/template-login-api-config.yaml > $SCRIPT_DIR/../login-api/login-api-config.yaml
 
 # Create cluster
 kind create cluster --config $SCRIPT_DIR/../kind-config.yaml --name ccu-cluster
 
 echo "Creating configmaps..."
-kubectl create configmap web-api-env --from-file=$SCRIPT_DIR/../web-api/.web-api-env
+kubectl create configmap web-api-env --from-file=.env=$SCRIPT_DIR/../web-api/.web-api-env
+kubectl create configmap login-api-env --from-file=.env=$SCRIPT_DIR/../login-api/.login-api-env
+kubectl create configmap login-api-db-preload-script --from-file=$SCRIPT_DIR/../login-api/preload/preload.sh
+kubectl create configmap login-api-db-preload --from-file=$SCRIPT_DIR/../login-api/preload
+
 
 # Apply web-api config
-echo "Applying web-api config..."
+echo "Applying deployment configs..."
 kubectl apply -f $SCRIPT_DIR/../web-api/web-api-config.yaml
+kubectl apply -f $SCRIPT_DIR/../login-api/login-api-config.yaml
+
 
